ce423eff41006be94f6262b47c2d788f73782d1e
GitJihyun <ayumu0214@naver.com>
2019-02-23 15:41:19 +0900

    layout

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Statusbar, TextInput, Dimensions,Platform} from 'react-native';
 
+const { height, width} = Dimensions.get("window")
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.title}>수안 지현 위시리스트</Text>
+        <View style={styles.card}>
+          <TextInput style = {styles.input} placeholder ={"새로 할것"}></TextInput>
+        </View>
       </View>
     );
   }
@@ -14,8 +18,36 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  title:{
+    color: "white",
+    fontSize: 30,
+    marginTop: 100,
+    fontWeight:"400",
+    marginBottom: 30  
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width-25,
+    borderTopLeftRadius :10,
+    borderTopRightRadius : 10,
+    ...Platform.select({
+      ios: {
+        shadowColor:"rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius:  10,
+        shadowOffset:{
+          height: -1,
+          width: 0
+        }
+      },
+      android:{
+        elevation: 3
+      }
+    })
+  }
 });
